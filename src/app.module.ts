1b3458dd9a8f0d850e77ed4372da71f249727e03
Yagnik-Gohil <gohilyagnik3@gmail.com>
2025-03-01 18:40:29 +0530

    feat: added user and token module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { envConfiguration } from 'config/configuration';
+import { envSchema } from 'config/validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { database } from 'config/database';
+import { TokenModule } from './modules/token/token.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${__dirname}/../../config/env/${process.env.NODE_ENV}.env`,
+      load: [envConfiguration],
+      isGlobal: true,
+      validationSchema: envSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => database(configService),
+      inject: [ConfigService],
+    }),
+    TokenModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
